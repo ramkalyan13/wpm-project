0fe8247563799ce2b917468bd88370923143be08
ramkalyan13 <keerthiramkalyan@gmail.com>
2023-10-16 20:33:39 +0530

    first commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+const ctrlLocations = require('../controller/locations');
+const ctrlOthers = require('../controller/others');
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
+module.exports = router;
